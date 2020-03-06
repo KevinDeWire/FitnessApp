f1b540d50be4fd0e55f90e8efecea6a85679d686
KevinD <kevind4272@yahoo.com>
2020-03-06 15:06:35 -0500

    Stated on step counter feature.

--- app/src/main/java/com/example/fitnessapp/MainActivity.java
+++ app/src/main/java/com/example/fitnessapp/MainActivity.java
@@ -2,13 +2,50 @@ package com.example.fitnessapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button logButton = findViewById(R.id.buttonLog);
+        Button chartButton = findViewById(R.id.buttonCharts);
+        Button stepCounterButton = findViewById(R.id.buttonStepCounter);
+
+        logButton.setOnClickListener(this);
+        chartButton.setOnClickListener(this);
+        stepCounterButton.setOnClickListener(this);
+
+
+
+
+    }
+
+    public void onClick(View v){
+        switch (v.getId()){
+            case R.id.buttonLog:
+                Intent intentLog = new Intent(this, ExerciseLog.class);
+                startActivity(intentLog);
+                break;
+            case R.id.buttonCharts:
+                Intent intentCharts = new Intent(this, Charts.class);
+                startActivity(intentCharts);
+                break;
+            case R.id.buttonStepCounter:
+                Intent intentStep = new Intent(this, StepCounter.class);
+                startActivity(intentStep);
+                break;
+        }
+    }
+
+    @Override
+    public void onPointerCaptureChanged(boolean hasCapture) {
+
     }
 }
