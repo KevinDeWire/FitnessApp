1f75e158ac085844897ebdc5b0efddaf19467dc1
Winston Shields <winstonjshields@gmail.com>
2020-03-08 15:35:08 -0400

    Sign in and sign up activity layout adjusted.

--- app/src/main/java/com/example/fitnessapp/SignUp.java
+++ app/src/main/java/com/example/fitnessapp/SignUp.java
@@ -1,5 +1,6 @@
 package com.example.fitnessapp;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
@@ -9,8 +10,13 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
 import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
-public class SignUp extends AppCompatActivity {
+public class SignUp extends AppCompatActivity implements View.OnClickListener {
+
+    Button signUpButton;
+    TextView signInLink;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,6 +24,22 @@ public class SignUp extends AppCompatActivity {
         setContentView(R.layout.activity_sign_up);
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+
+        signUpButton = findViewById(R.id.signUpButton);
+        signInLink = findViewById(R.id.signInLink);
+
+        signUpButton.setOnClickListener(this);
+        signInLink.setOnClickListener(this);
     }
 
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.signUpButton:
+                break;
+            case R.id.signInLink:
+                Intent signInActivity = new Intent(this, SignIn.class);
+                startActivity(signInActivity);
+        }
+    }
 }
