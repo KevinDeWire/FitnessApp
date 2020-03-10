fed2d6cd65c6e69b8cbba4836749b686bbda22dc
Winston Shields <winstonjshields@gmail.com>
2020-03-10 19:46:03 -0400

    Username from database is displayed on main friends page

--- app/src/main/java/com/example/fitnessapp/Friends.java
+++ app/src/main/java/com/example/fitnessapp/Friends.java
@@ -9,7 +9,11 @@ import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.google.android.material.snackbar.Snackbar;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.EventListener;
 import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.FirebaseFirestoreException;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
@@ -19,17 +23,23 @@ import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.Spinner;
 import android.widget.TextView;
 import android.widget.Toast;
+
+import javax.annotation.Nullable;
 
 public class Friends extends AppCompatActivity implements View.OnClickListener {
 
     TextView username;
     TextView resendVerification;
+    ImageView profilePicture;
 
     FirebaseAuth firebaseAuth;
     FirebaseFirestore firebaseFirestore;
+
+    String userId;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -40,6 +50,7 @@ public class Friends extends AppCompatActivity implements View.OnClickListener {
 
         resendVerification = findViewById(R.id.notVerified);
         username = findViewById(R.id.display_username);
+        profilePicture = findViewById(R.id.profilePicture);
 
         firebaseAuth = FirebaseAuth.getInstance();
         firebaseFirestore = FirebaseFirestore.getInstance();
@@ -49,6 +60,10 @@ public class Friends extends AppCompatActivity implements View.OnClickListener {
 
         FirebaseUser firebaseUser = firebaseAuth.getCurrentUser();
 
+        // Get the user's ID.
+        userId = firebaseUser.getUid();
+
+
         if (!firebaseUser.isEmailVerified()) {
             // If the user's email address is not verified, display resend
             // verification email link.
@@ -56,7 +71,25 @@ public class Friends extends AppCompatActivity implements View.OnClickListener {
 
             // Set the profile link to false as well.
             username.setEnabled(false);
+
+            // If the user's email is not verified, make the profile picture
+            // invisible.
+            profilePicture.setVisibility(View.INVISIBLE);
         }
+
+        DocumentReference documentReference = firebaseFirestore.collection("users")
+                .document(userId);
+
+        // Listen to the data in the FireBase database.
+        documentReference.addSnapshotListener(this, new EventListener<DocumentSnapshot>() {
+            @Override
+            public void onEvent(@Nullable DocumentSnapshot documentSnapshot,
+                                @Nullable FirebaseFirestoreException e) {
+                // Set the username from the database on the screen.
+                username.setText(documentSnapshot.getString("username"));
+            }
+        });
+
     }
 
     @Override
