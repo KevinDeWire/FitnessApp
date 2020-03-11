c93af0a5823475c5f271982bab23a195039249c0
Winston Shields <winstonjshields@gmail.com>
2020-03-10 20:51:00 -0400

    User profile activity worked on. Email and username shown.

--- app/src/main/java/com/example/fitnessapp/UserProfile.java
+++ app/src/main/java/com/example/fitnessapp/UserProfile.java
@@ -4,13 +4,31 @@ import android.os.Bundle;
 
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.google.android.material.snackbar.Snackbar;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.EventListener;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.FirebaseFirestoreException;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
 import android.view.View;
+import android.widget.TextView;
+
+import javax.annotation.Nullable;
 
 public class UserProfile extends AppCompatActivity {
+    TextView username;
+    TextView email;
+
+    FirebaseAuth firebaseAuth;
+    FirebaseFirestore firebaseFirestore;
+    FirebaseUser firebaseUser;
+
+    String userId;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,5 +36,32 @@ public class UserProfile extends AppCompatActivity {
         setContentView(R.layout.activity_user_profile);
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+
+        username = findViewById(R.id.display_username);
+        email = findViewById(R.id.display_email);
+
+        firebaseAuth = FirebaseAuth.getInstance();
+        firebaseUser = firebaseAuth.getCurrentUser();
+
+        firebaseFirestore = FirebaseFirestore.getInstance();
+
+        userId = firebaseUser.getUid();
+
+        // Make a reference to the users document in Firebase.
+        final DocumentReference documentReference = firebaseFirestore.collection("users")
+                .document(userId);
+
+        // Listen to the data in the FireBase database.
+        documentReference.addSnapshotListener(this, new EventListener<DocumentSnapshot>() {
+            @Override
+            public void onEvent(@Nullable DocumentSnapshot documentSnapshot,
+                                @Nullable FirebaseFirestoreException e) {
+                // Set the username from the database onto the profile.
+                username.setText(documentSnapshot.getString("username"));
+                // Set the email from the database on the profile.
+                email.setText(documentSnapshot.getString("email"));
+            }
+        });
+
     }
 }
