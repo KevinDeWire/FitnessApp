c48243a13d7255610d87f19b57c6ccf1daeb90d5
Winston Shields <winstonjshields@gmail.com>
2020-03-18 16:42:02 -0400

    Notification takes user to activity.

--- app/src/main/java/com/example/fitnessapp/MyFirebaseMessagingService.java
+++ app/src/main/java/com/example/fitnessapp/MyFirebaseMessagingService.java
@@ -5,6 +5,8 @@ import android.app.NotificationManager;
 import android.app.PendingIntent;
 import android.content.Intent;
 import android.os.Build;
+import android.util.Log;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.RequiresApi;
@@ -16,6 +18,7 @@ import com.google.firebase.messaging.RemoteMessage;
 public class MyFirebaseMessagingService extends FirebaseMessagingService {
 
     private static final String CHANNEL_ID = "friend_requests";
+    private static final String TAG = "fromUserID";
 
     /**
      * Receive notifications in the foreground.
