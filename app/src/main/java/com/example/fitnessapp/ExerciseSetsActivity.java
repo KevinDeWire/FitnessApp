f8483633127eb1e088461fe8571981fd801a2246
Winston Shields <winstonjshields@gmail.com>
2020-03-31 14:28:45 -0400

    Fixed errors as well as added invalid inputs to logs.

--- app/src/main/java/com/example/fitnessapp/ExerciseSetsActivity.java
+++ app/src/main/java/com/example/fitnessapp/ExerciseSetsActivity.java
@@ -11,6 +11,7 @@ import androidx.appcompat.widget.Toolbar;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import android.text.TextUtils;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.AdapterView;
@@ -44,7 +45,6 @@ public class ExerciseSetsActivity extends AppCompatActivity implements View.OnCl
         setContentView(R.layout.activity_exercise_sets);
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
-
 
         // Display the back button.
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
@@ -85,16 +85,26 @@ public class ExerciseSetsActivity extends AppCompatActivity implements View.OnCl
         String reps = mEnterReps.getText().toString().trim();
         String rpe = mEnterRpe.getText().toString().trim();
 
-        // Create an instance of the exercise set and fill the attributes.
+        // Create an instance of the exercise set.
         ExerciseSet set = new ExerciseSet();
-        set.setWeight(Double.parseDouble(weight));
-        set.setMetric(metric);
-        set.setReps(Integer.parseInt(reps));
-        set.setRpe(Double.parseDouble(rpe));
 
-        // Add the set to the list of exercise sets.
-        exerciseSets.add(set);
-        mAdapter.updateData(exerciseSets);
+        if (checkIfEmpty(weight, mEnterWeight) && checkIfEmpty(reps, mEnterReps)
+                && checkIfEmpty(rpe, mEnterRpe)) {
+            if (Double.parseDouble(rpe) > 10 || Double.parseDouble(rpe) <= 0) {
+                // If the entered RPE is less than 1 or greater than 10, set an error message.
+                mEnterRpe.setError("RPE must be from 1-10");
+            } else {
+                // If there are no errors, add the attributes to the set.
+                set.setWeight(Double.parseDouble(weight));
+                set.setMetric(metric);
+                set.setReps(Integer.parseInt(reps));
+                set.setRpe(Double.parseDouble(rpe));
+
+                // Add the set to the list of exercise sets.
+                exerciseSets.add(set);
+                mAdapter.updateData(exerciseSets);
+            }
+        }
     }
 
     @Override
@@ -112,5 +122,76 @@ public class ExerciseSetsActivity extends AppCompatActivity implements View.OnCl
     @Override
     public void onItemClick(View view, int position) {
 
+    }
+
+    /**
+     * Check if string is numeric.
+     *
+     * @param stringNumber String
+     * @return boolean
+     */
+    private boolean isNumeric(String stringNumber) {
+        if (stringNumber == null) {
+            // If string is null, return false.
+            return false;
+        }
+        try {
+            // If double is successfully parsed from the string, return true.
+            Double.parseDouble(stringNumber);
+            return true;
+        } catch (NumberFormatException e) {
+            // If there is an error, return false.
+            return false;
+        }
+    }
+
+    /**
+     * If string is empty, set error message.
+     *
+     * @param string   String
+     * @param textView TextView
+     * @return boolean
+     */
+    private boolean checkIfEmpty(String string, TextView textView) {
+        if (TextUtils.isEmpty(string)) {
+            textView.setError("You must enter a value.");
+            return false;
+        } else {
+            checkIfNumeric(string, textView);
+            return true;
+        }
+    }
+
+    /**
+     * If string isn't numeric, set an error message.
+     *
+     * @param string   string of TextView element
+     * @param textView TextView element for string
+     * @return boolean
+     */
+    private boolean checkIfNumeric(String string, TextView textView) {
+        if (!isNumeric(string)) {
+            textView.setError("You must enter a numeric value.");
+            return false;
+        } else {
+            checkIfPositive(string, textView);
+            return true;
+        }
+    }
+
+    /**
+     * If number is not greater than zero, set an error message.
+     *
+     * @param stringNumber number
+     * @param textView     TextView element for number.
+     * @return boolean
+     */
+    private boolean checkIfPositive(String stringNumber, TextView textView) {
+        if (Double.parseDouble(stringNumber) <= 0) {
+            textView.setError("Value must be greater than zero.");
+            return false;
+        } else {
+            return true;
+        }
     }
 }
